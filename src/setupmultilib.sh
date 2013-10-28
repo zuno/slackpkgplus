ca10521862d97891fad2b4f02a229c0657576efa
David Spencer <baildon.research@googlemail.com>
2013-10-28 23:16:36 +0000

    Typos and minor edits (part 3)
     - setupmultilib.sh: typos in echo messages

--- src/setupmultilib.sh
+++ src/setupmultilib.sh
@@ -13,7 +13,7 @@ if [ $ISX64 -ne 1 ];then
 fi
 SVER=$(grep -v ^\# /etc/slackpkg/mirrors|sed -r 's,^.*/slackware64-(current|14.1|14.0|13.37|13.0)/,\1,'|head -1)
 if [ -z "$SVER" ];then
-  echo "I can't detect you Slackware version."
+  echo "I can't detect your Slackware version."
   echo "Which Slackware version are you running? (current/14.1/14.0/13.37/13.0)"
   read $SVER
 fi
@@ -23,7 +23,7 @@ if ! echo $SVER|egrep -q '^(current|14.1|14.0|13.37|13.0)$';then
 fi
 
 if grep -q -e '^PKGS_PRIORITY=.* multilib:\.\* .*$' -e '^MIRRORPLUS..multilib..=.*multilib.*' /etc/slackpkg/slackpkgplus.conf;then
-  echo "slackpkg+ seem to be already configured for multilib support. Would you like to remove multilib support from the configuration? (y/N)"
+  echo "slackpkg+ seems to be already configured for multilib support. Would you like to remove multilib support from the configuration? (y/N)"
   read ANS
   if [ "$ANS" == "y" -o "$ANS" == "Y" ];then
     cp /etc/slackpkg/slackpkgplus.conf /etc/slackpkg/slackpkgplus.conf.backup
@@ -33,7 +33,7 @@ if grep -q -e '^PKGS_PRIORITY=.* multilib:\.\* .*$' -e '^MIRRORPLUS..multilib..=
       -e 's/^(MIRRORPLUS..multilib..=.*multilib.*)$/#\1/' \
       -e 's/^REPOPLUS=(.*) multilib (.*)/REPOPLUS=\1 \2/' \
       -e 's/^(REPOPLUS=\( +\).*)$/#\1/' /etc/slackpkg/slackpkgplus.conf
-    echo "multilib support has been removed from slackpkg+. Now you need to remove the installed packages (using slackpkg)."
+    echo "Multilib support has been removed from slackpkg+. Now you need to remove the installed packages (using slackpkg)."
     echo "Would you like this script to run slackpkg for you and remove the Multilib packages? (y/N)"
     read ANS
     if [ "$ANS" == "y" -o "$ANS" == "Y" ];then
@@ -55,7 +55,7 @@ if grep -q -e '^PKGS_PRIORITY=.* multilib:\.\* .*$' -e '^MIRRORPLUS..multilib..=
   fi
 fi
 
-echo "Would you setup slackpkg+ to add multilib support? (y/N)"
+echo "Would you like to setup slackpkg+ to add multilib support? (y/N)"
 read ANS
 if [ "$ANS" == "y" -o "$ANS" == "Y" ];then
   MULTILIBREPO="MIRRORPLUS['multilib']="$(grep -m1 '> multilib: ' repositories.txt|awk '{print $3}'|sed "s/{.*}/$SVER/")
@@ -68,7 +68,7 @@ if [ "$ANS" == "y" -o "$ANS" == "Y" ];then
   sed -i -r -e 's|^(REPOPLUS=.*)$|\1\n'"$MULTILIBREPO|" /etc/slackpkg/slackpkgplus.conf
   sed -i.backup -r -e 's/^(\[0-9\]\+compat32)$/\#\1/' /etc/slackpkg/blacklist
   echo "slackpkg+ is now configured for multilib support."
-  echo "Do you want to install then multilib now? (y/N)"
+  echo "Do you want to install the multilib now? (y/N)"
   read ANS
   if [ "$ANS" == "y" -o "$ANS" == "Y" ];then
     slackpkg update gpg
